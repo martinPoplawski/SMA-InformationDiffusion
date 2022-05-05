72df075ad878f3af8a86eb1f1b145fb2becf1721
Marco Cacciatore <marco.cacciatore@students.unibe.ch>
2022-05-05 16:14:07 +0200

    added progressbar to helpers

--- src/Helpers/Helpers.py
+++ src/Helpers/Helpers.py
@@ -7,3 +7,34 @@ def print(message):
     """
     if Config.verbose:
         pp(message)
+
+def progress(amount, max, steps=1, length=100):
+    """
+    prints a progress bar
+    """
+    if Config.verbose:
+        max -= 1
+        if amount > max:
+            print("cant print progress bar")
+            return                    
+        if amount % steps == 0 or amount == max:                  
+            percent = int(amount *length / max)
+            estimatedProgress = ("~" if steps != 1 else "") + f"{amount:,}/{max:,}".replace(",", "'")
+            prog = "[" + "#" * percent + "-" * (length - percent)
+            endNl = "\n" if amount == max else ""                       
+            pp("\r" + prog + f"]{percent}% {estimatedProgress}", end=endNl)
+
+
+
+if __name__ == "__main__":
+    """
+    test cases
+    """
+
+    Config.verbose = True
+    print("this is a test")
+    progress(50, 100)
+    print("")
+    progress(50, 100)
+    progress(100, 100)    
+    progress(50000, 100000, steps=1000)
