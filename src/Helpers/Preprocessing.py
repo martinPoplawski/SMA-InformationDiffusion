18492426b165ec246327c577dfe6969a088a7298
Marco Cacciatore <marco.cacciatore@students.unibe.ch>
2022-05-05 16:14:36 +0200

    added weights to edges and use progress bar

--- src/Helpers/Preprocessing.py
+++ src/Helpers/Preprocessing.py
@@ -1,12 +1,14 @@
 import csv  
 import time
-from Helpers import print
+import Config 
+from Helpers import print, progress
 
 
 class Tweet:
     person1: int
     person2: int
     timestamp: int
+    weight = 1
 
     def __init__(self, person1, person2, timestamp=0):
         self.person1 = person1
@@ -28,27 +30,48 @@ class Tweet:
         return f"{self.person1=}, {self.person2=}, {self.timestamp=}"
  
 
+class Tweets:
+    tweets: set[Tweet]
+    def __init__(self):
+        self.tweets = set()
+
+    def add(self, tweet: Tweet):
+        if tweet not in self.tweets:
+            self.tweets.add(tweet)
+        else:
+            tweet.weight += 1
+            self.tweets.remove(tweet)
+            self.tweets.add(tweet)
+
 def combineFiles():
     with open("src/data/higgs-activity_time.txt") as activity_file, open("src/data/higgs-social_network.edgelist") as social_file:
         activity = csv.reader(activity_file)
         social = csv.reader(social_file)
 
-        tweets = set()
-
-        for edge in activity:
+        tweets = Tweets()
+        print("reading activity")
+        for i, edge in enumerate(activity):
             edge = edge[0].split(" ")
-            tweets.add(Tweet(edge[0], edge[1], edge[2]))
-        print(f"read activity: {len(tweets)}")
-
-        for edge in social:
-            edge = edge[0].split(" ")
-            tweets.add(Tweet(edge[0], edge[1]))
-        print(f"read social: {len(tweets)}")
+            tweets.add(Tweet(edge[0], edge[1], edge[2]))                            
+            progress(i, 563069, steps=5000)
+        print(f"read activity: {len(tweets.tweets)}")
         
-
-        with open(f"src/data/preprocessed_{int(time.time())}.csv", "w", newline="") as file:
+        print("reading social")
+        for i, edge in enumerate(social):
+            edge = edge[0].split(" ")
+            tweets.add(Tweet(edge[0], edge[1]))                          
+            progress(i, 14855841, steps=10000) 
+        print(f"read social: {len(tweets.tweets)}")
+        
+        filename = f"preprocessed_{int(time.time())}.csv"
+        with open(f"src/data/{filename}", "w", newline="") as file:
             data = csv.writer(file)
-            data.writerows([i.person1, i.person2, i.timestamp] for i in tweets)
+            print(f"writing to csv {filename}")
+            data.writerow(["person1", "person2", "weight", "timestamp"])
+            for i, tweet in enumerate(tweets.tweets):
+                data.writerow([tweet.person1, tweet.person2, tweet.weight, tweet.timestamp])
+                progress(i, len(tweets.tweets), steps=1000)            
+
 
 
 def openFile(file) -> set[Tweet]:
@@ -69,9 +92,12 @@ def checkFile(file):
     print(f"{count=}, {len(tweets)=}")
 
 
+if __name__ == "__main__":
+    Config.verbose = True
+    combineFiles()
 
 #combineFiles()
-checkFile("src/data/preprocessed_1648560717.csv") #file is correct
+#checkFile("src/data/preprocessed_1648560717.csv") #file is correct
 
         
         
