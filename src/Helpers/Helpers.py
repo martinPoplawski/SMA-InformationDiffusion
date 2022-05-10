728c061c68afccebe81424729525305b88faf4c9
Cacciu <m.t.c1@hotmail.com>
2022-05-10 10:00:02 +0200

    bug fix in percent calculation and progress display when length is not 100

--- src/Helpers/Helpers.py
+++ src/Helpers/Helpers.py
@@ -23,16 +23,15 @@ def progress(amount, max, steps=1, length=None):
             length, _ = shutil.get_terminal_size()
             length = length - 8 #fixed amount of characters
             length = length - len(f"{(max+1):,}") -len(f"{amount:,}")
-        max -= 1
-        if amount > max+1:
+        if amount > max:
             pp("cant print progress bar")
             return                    
-        if amount % steps == 0 or amount == max:                  
-            percent = int(amount *length / max)
-            estimatedProgress = ("~" if steps != 1 else "") + f"{amount:,}/{(max+1):,}".replace(",", "'")
-            prog = "[" + "#" * percent + "-" * (length - percent)
-            endNl = "\n" if amount == max else ""                       
-            pp("\r" + prog + f"]{percent}% {estimatedProgress}", end=endNl)
+        if amount % steps == 0 or amount == max-1:                  
+            percent = int(amount *100 / max)
+            estimatedProgress = ("~" if steps != 1 else "") + f"{amount:,}/{(max):,}".replace(",", "'")
+            prog = "[" + "#" * int(percent/100*length) + "-" * int(length - (percent/100*length)) + "]"
+            endNl = "\n" if amount == max-1 else ""                       
+            pp("\r" + prog + f"{percent}% {estimatedProgress}", end=endNl)
 
 
 
@@ -45,6 +44,11 @@ if __name__ == "__main__":
     print("this is a test")
     progress(50, 100)
     print("")
+
     progress(50, 100)
-    progress(100, 100)    
+    progress(100, 100)   
+
     progress(50000, 100000, steps=1000)
+    print("")
+
+    progress(812811, 3251245) #about 25%
