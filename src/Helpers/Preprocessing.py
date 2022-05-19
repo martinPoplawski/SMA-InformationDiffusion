a93e8fb9310c537b70c30f69129e025debe29ada
Marco Cacciatore <marco.cacciatore@students.unibe.ch>
2022-05-19 19:15:25 +0200

    added vertexlist

--- src/Helpers/Preprocessing.py
+++ src/Helpers/Preprocessing.py
@@ -10,18 +10,20 @@ class Tweet:
     timestamp: int
     weight = 1
 
-    def __init__(self, person1, person2, timestamp=0):
-        self.person1 = person1
-        self.person2 = person2
-        self.timestamp = timestamp
-
-
+    def __init__(self, person1, person2, timestamp=0, weight=1):
+        self.person1 = int(person1)
+        self.person2 = int(person2)
+        self.timestamp = int(timestamp)
+        self.weight = int(weight)
     
     def __eq__(self, other):
         """
         checks if the persons are the same and the tweettype is the same
         """
         return other.person1 == self.person1 and other.person2 == self.person2
+    
+    def __ne__(self, other):
+        return not self.__eq__(other)
 
     def __hash__(self) -> int:
         return int(str(self.person1) + "0000000000" + str(self.person2))
@@ -43,6 +45,37 @@ class Tweets:
             self.tweets.remove(tweet)
             self.tweets.add(tweet)
 
+class Vertex:
+    id: int
+    weight: int
+
+    def __init__(self, id, weight):
+        self.id = int(id)
+        self.weight = int(weight)
+    
+    def __hash__(self) -> int:
+        return self.id
+
+    def __eq__(self, other):
+        return self.id == other.id
+    
+    def __ne__(self, other):
+        return self.id != other.id
+
+class Vertices:
+    vertices: set[Vertex]
+    def __init__(self):
+        self.vertices = set()
+
+    def add(self, vertex: Vertex):
+        if vertex not in self.vertices:
+            self.vertices.add(vertex)
+        else:
+            vertex.weight += 1
+            self.vertices.remove(vertex)
+            self.vertices.add(vertex)
+
+
 def combineFiles():
     with open("src/data/higgs-activity_time.txt") as activity_file, open("src/data/higgs-social_network.edgelist") as social_file:
         activity = csv.reader(activity_file)
@@ -53,24 +86,39 @@ def combineFiles():
         for i, edge in enumerate(activity):
             edge = edge[0].split(" ")
             tweets.add(Tweet(edge[0], edge[1], edge[2]))                            
-            progress(i, 563069, steps=5000)
-        print(f"read activity: {len(tweets.tweets)}")
+            progress(i, 563069, steps=5000)                    
         
         print("reading social")
         for i, edge in enumerate(social):
             edge = edge[0].split(" ")
             tweets.add(Tweet(edge[0], edge[1]))                          
-            progress(i, 14855841, steps=10000) 
-        print(f"read social: {len(tweets.tweets)}")
+            progress(i, 14855841, steps=10000)                     
         
-        filename = f"preprocessed_{int(time.time())}.csv"
-        with open(f"src/data/{filename}", "w", newline="") as file:
+        filename = f"preprocessed_{int(time.time())}"
+        with open(f"src/data/{filename}.csv", "w", newline="") as file:
             data = csv.writer(file)
-            print(f"writing to csv {filename}")
-            data.writerow(["person1", "person2", "weight", "timestamp"])
+            print(f"writing edgelist to csv {filename}.csv")
+            #data.writerow(["person1", "person2", "weight", "timestamp"])
             for i, tweet in enumerate(tweets.tweets):
                 data.writerow([tweet.person1, tweet.person2, tweet.weight, tweet.timestamp])
-                progress(i, len(tweets.tweets), steps=1000)            
+                progress(i, len(tweets.tweets), steps=1000)       
+
+        generateVertexListFromTweets(tweets.tweets, filename)    
+
+def generateVertexListFromTweets(tweets, filename):
+    vertices = Vertices()
+    for i, tweet in enumerate(tweets):                
+        vertices.add(Vertex(tweet.person1, 1))
+        vertices.add(Vertex(tweet.person2, 1))
+        progress(i, len(tweets), steps=1000)
+    #store vertices in csv
+    with open(f"src/data/{filename}_vertexlist.csv", "w", newline="") as file:
+        data = csv.writer(file)        
+        #write vertices and their weights to csv
+        print(f"writing vertexlist to {filename}_vertexlist.csv")
+        for i, vertex in enumerate(vertices.vertices):
+            data.writerow([vertex.id, vertex.weight])
+            progress(i, len(vertices.vertices), steps=1000)
 
 
 
@@ -91,13 +139,35 @@ def checkFile(file):
             count += 1
     print(f"{count=}, {len(tweets)=}")
 
+def fuuuuuuck():
+    """
+    this function can be used if you fucked up the vertexlist generation
+    it allows you to generate the vertexlist from the edgelist csv
+    """
+    filename = "preprocessed_1652172596"
+    with open(f"src/data/{filename}.csv") as file:        
+        vertices = set()
+        reader = csv.reader(file)
+        for i, edge in enumerate(reader):            
+            vertices.add(Vertex(edge[0], 1))
+            vertices.add(Vertex(edge[1], 1))
+            progress(i, 15056958, steps=1000)                           
+
+    #store vertices in csv
+    with open(f"src/data/{filename}_vertexlist.csv", "w", newline="") as file:
+        data = csv.writer(file)        
+        #write vertices and their weights to csv
+        print(f"writing vertexlist to {filename}_vertexlist.csv")
+        for i, vertex in enumerate(vertices):
+            data.writerow([vertex.id, vertex.weight])
+            progress(i, len(vertices), steps=1000)
+
 
 if __name__ == "__main__":
     Config.verbose = True
+    #fuuuuuuck()
+    print("done")
     combineFiles()
-
-#combineFiles()
-#checkFile("src/data/preprocessed_1648560717.csv") #file is correct
 
         
         
